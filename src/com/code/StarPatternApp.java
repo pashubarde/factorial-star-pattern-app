42a709cdf9d196f9386346ac0f4b5e048625704a
Prashant <pashu.barde@gmail.com>
2023-05-15 16:21:50 +0530

    complete star pattern example

--- src/com/code/StarPatternApp.java
+++ src/com/code/StarPatternApp.java
@@ -3,15 +3,20 @@ package com.code;
 public class StarPatternApp {
 
 	public static void main(String[] args) {
-		
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				while()
-				System.out.println("*");
-				
+
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+
+				System.out.print("*");
 			}
-			
-			
+			System.out.println();
+		}
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 4; j >= i; j--) {
+
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 	}
 
