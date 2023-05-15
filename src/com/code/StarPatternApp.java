192bdb46b6116cbce2088d165169bd8c5dee9782
Prashant <pashu.barde@gmail.com>
2023-05-15 16:02:48 +0530

    made changes

--- src/com/code/StarPatternApp.java
+++ src/com/code/StarPatternApp.java
@@ -4,8 +4,9 @@ public class StarPatternApp {
 
 	public static void main(String[] args) {
 		
-		for (int i = 5; i >=1 ; i--) {
+		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
+				while()
 				System.out.println("*");
 				
 			}
